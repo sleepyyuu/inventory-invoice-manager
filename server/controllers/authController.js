43a0c267b160b002058e2e77a619780f3bc2e9f9
Yu <k28143995@gmail.com>
2022-05-14 17:48:52 -0700

    jwt sign object fix

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,30 +1,32 @@
 const passport = require("passport");
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
+const User = require("../models/user");
 
 exports.login_post = function (req, res, next) {
   passport.authenticate("login", { session: false }, function (err, user, info) {
     if (err) {
       return next(err);
     }
-    req.login(user, { sessions: false }, function (err) {
+    req.login(user, { sessions: false }, async function (err) {
       if (err) {
-        console.log(err);
-        return res.status(400).json();
+        return res.status(400).json({ message: info.message });
       }
+      const dbUser = await User.findOne({ username: user.username }).exec();
       const cookies = req.cookies;
-      const accessToken = jwt.sign({ username: user.username }, process.env.JWT_TOKEN, { expiresIn: "10m" });
-      const newRefreshToken = jwt.sign({ username: user.username }, process.env.REFRESH_TOKEN_SECRET, {
+      const accessToken = jwt.sign({ username: dbUser.username }, process.env.JWT_TOKEN, { expiresIn: "10m" });
+      const newRefreshToken = jwt.sign({ username: dbUser.username }, process.env.REFRESH_TOKEN_SECRET, {
         expiresIn: "10d",
       });
 
       const newRefreshTokenArray = !cookies?.jwt
-        ? user.refreshToken
-        : user.refreshToken.filter((token) => token !== cookies.jwt);
+        ? dbUser.refreshToken
+        : dbUser.refreshToken.filter((token) => token !== cookies.jwt);
       if (cookies.jwt) {
         res.clearCookie("jwt", { httpOnly: true, sameSite: "Lax", secure: true });
       }
-      user.refreshToken = [newRefreshTokenArray, newRefreshToken];
+      dbUser.refreshToken = [...newRefreshTokenArray, newRefreshToken];
+      await dbUser.save();
       res.cookie("jwt", newRefreshToken, {
         httpOnly: true,
         secure: true,
