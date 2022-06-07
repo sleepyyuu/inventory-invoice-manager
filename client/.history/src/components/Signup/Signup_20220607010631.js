5245a7192a8f9e4b25cc9e7d417e566cae7b5a96
Yu <k28143995@gmail.com>
2022-06-07 02:36:03 -0700

    update

---
+++ client/.history/src/components/Signup/Signup_20220607010631.js
@@ -0,0 +1,74 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { userRequest } from "../../api/api";
+import "./Signup.css";
+import { useNavigate } from "react-router-dom";
+export default function Signup() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  const signupUser = async (e) => {
+    e.preventDefault();
+    const response = await userRequest(username, password, "/auth/signup");
+    if (response.error != null) {
+      //error so do something
+      setError(response.error);
+    } else {
+      setError(null);
+      //not error, setstate to success, router link to dashboard
+    }
+  };
+
+  return (
+    <div className="signupPage">
+      <div className="signupContainer">
+        {" "}
+        <h2>Signup</h2>
+        {error ? <div>{error}</div> : <div></div>}
+        <form className="signupForm" onSubmit={signupUser}>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            name="username"
+            id="username"
+            className="signupInput"
+            onChange={(e) => {
+              setUsername(e.target.value);
+            }}
+            value={username}
+          ></input>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            className="signupInput"
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            value={password}
+          ></input>
+          <input type="submit" value="Signup" className="signupButton"></input>
+        </form>
+        <div
+          className="loginLink"
+          onClick={() => {
+            navigate("/login");
+          }}
+        >
+          Have an account? Log in
+        </div> */
+        <div
+          className="loginLink"
+          onClick={() => {
+            navigate("/login");
+          }}
+        >
+          Have an account? Log in
+        </div>
+      </div>
+    </div>
+  );
+}
