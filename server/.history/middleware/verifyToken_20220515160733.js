bad7934738bb125888d67c78e29eed75132f0f93
sleepyyuu <96076501+sleepyyuu@users.noreply.github.com>
2022-06-14 16:33:30 -0700

    Delete server/.history directory

--- server/.history/middleware/verifyToken_20220515160733.js
+++
@@ -1,9 +0,0 @@
-const jwt = require("jsonwebtoken");
-
-const verifyToken = (req, res, next) => {
-  const authHeader = req.headers["authorization"];
-  if (!authHeader) {
-    return res.sendStatus(404);
-  }
-  const accessToken = authHeader.split(" ")[1];
-};
